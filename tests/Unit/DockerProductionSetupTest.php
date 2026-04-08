e0e81798a4b2a9d521c90d895005fe7abbf01b1d
matt <rdeveloper1925@gmail.com>
2026-04-08 14:06:19 -0400

    Initial Commit

---
+++ tests/Unit/DockerProductionSetupTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class DockerProductionSetupTest extends TestCase
+{
+    public function test_docker_production_files_exist(): void
+    {
+        $root = dirname(__DIR__, 2);
+        $this->assertFileExists($root.'/docker-compose.yml');
+        $this->assertFileExists($root.'/.dockerignore');
+        $this->assertFileExists($root.'/docker/common/php-fpm/Dockerfile');
+        $this->assertFileExists($root.'/docker/common/php-fpm/conf.d/20-status-path.conf');
+        $this->assertFileExists($root.'/docker/production/php-fpm/entrypoint.sh');
+        $this->assertFileExists($root.'/docker/production/nginx/Dockerfile');
+        $this->assertFileExists($root.'/docker/production/nginx/nginx.conf');
+    }
+}
